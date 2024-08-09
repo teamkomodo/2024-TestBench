ac32b2fee2a456e94142dc9859d8e04ae6770955
Arjun-P-R <arjunrhg@gmail.com>
2024-08-08 19:46:17 -0600

    Get code ready

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,15 +9,13 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.TestMotorSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 import static frc.robot.Constants.OperatorConstants.*;
-
-//this is yar
 
 @SuppressWarnings("unused")
 /**
@@ -29,7 +27,7 @@ import static frc.robot.Constants.OperatorConstants.*;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem(32);
-  private final TestMotorSubsystem testmotorSubsystem = new TestMotorSubsystem();
+  private final ShooterSubsystem testmotorSubsystem = new ShooterSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
